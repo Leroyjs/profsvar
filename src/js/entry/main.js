c502d8f220e63182273dbb5f245750c6a930d926
Никита <nikita@imac-dmitrij.ravoskhod.local>
2021-06-02 18:06:24 +0500

    responce

--- src/js/entry/main.js
+++ src/js/entry/main.js
@@ -424,6 +424,22 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }
   })();
+  (function initAncors() {
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      });
+    }
+  })();
 });
 
 const fakeJSONForCases = {
